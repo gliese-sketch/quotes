b064538082b16856813b33c74a42c4af3d3cb9d7
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-25 07:30:59 +0000

    add SingleQuote

--- src/components/SingleQuote.jsx
+++ src/components/SingleQuote.jsx
@@ -1,5 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Button } from "@/components/ui/button";
+
 function SingleQuote() {
-  return <div>SingleQuote</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-lg">I am a quote</CardTitle>
+        <CardDescription>Total Likes: 0</CardDescription>
+      </CardHeader>
+      <CardFooter className="flex gap-2">
+        <Button variant="outline">👍</Button>
+        <Button variant="outline">👎</Button>
+        <Button variant="destructive">Delete</Button>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export default SingleQuote;
