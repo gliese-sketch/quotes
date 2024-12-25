68861fe9ba65238ea523e6221b23045749c48031
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-25 07:17:43 +0000

    add AddQuote

--- src/components/AddQuote.jsx
+++ src/components/AddQuote.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 function AddQuote() {
+  const [inputValue, setIntputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent Form Refresh
+
+    if (!inputValue.trim()) return; // Return if input empty
+
+    console.log(inputValue);
+
+    setIntputValue(""); // Reset the input field
+  };
+
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <Input type="text" placeholder="Add a Quote" className="mb-2" />
+    <form onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        placeholder="Add a Quote"
+        className="mb-2"
+        onChange={(e) => setIntputValue(e.target.value)}
+        value={inputValue}
+      />
 
       <div className="flex gap-2">
-        <Button onClick={() => console.log("Add button")}>Add</Button>
-        <Button onClick={() => console.log("Sort button")}>Sort</Button>
+        <Button>Add</Button>
+        <Button>Sort</Button>
       </div>
     </form>
   );
